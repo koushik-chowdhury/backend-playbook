47e53871bcf5673d61596571bf37d3605993b936
Koushik Chowdhury <csakoushik@gmail.com>
2025-05-08 22:48:19 +0530

    add patch and delete route

--- rest-api/index.js
+++ rest-api/index.js
@@ -17,6 +17,25 @@ app.get('/users',(req,res)=>{
 app.get('/api/users',(req,res)=>{
     return res.json(users)
 })
+
+app.route('/api/users/:id')
+.get((req,res)=>{
+    const id = Number(req.params.id)
+    const user = users.find(user=>user.id===id)
+    return res.json(user)
+})
+.put((req,res)=>{
+    return res.json({stats:'pending'})
+})
+.delete((req,res)=>{
+    return res.json({status:'pending'})
+})
+
+app.post('/api/users',(req,res)=>{
+    return res.json({status:'pending'})
+})
+
+
 app.listen(PORT, ()=>{
     console.log(`Server started at ${PORT}`)
 })
